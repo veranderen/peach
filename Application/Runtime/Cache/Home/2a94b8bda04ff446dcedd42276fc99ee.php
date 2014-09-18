0ac8d805f6a0262a24cfd6e811b951085a53163e
veranderen <pufan.cn@gmail.com>
2014-09-19 00:08:16 +0800

    增加主页图片和文字表述

--- Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -26,47 +26,59 @@ and open the template in the editor.
                 </nav>
             </div>
         </div>
-        <h1 class="address">易县</h1>
-        <div class="m-photo">
-            <img src="Public/img/易水湖3.jpg" height="360" width="680">           
+        <h1 class="address">千年古县－易县</h1>
+        <div class="first-screen clearfix">
+            <div class="m-photo">
+                <img src="Public/img/易水湖.jpg" height="360" width="680">           
+            </div>
+            <div class="p-aside">
+                <h3>易县印象</h3>
+                <p>最佳旅游季节 春秋两季</p>
+                <p>建议游玩天数 2天</p>
+                <p>
+                    易县位于河北省中西部，保定市西北部，因易水而得名，是著名的革命老区，是河北省文化名城，
+                    是中国绿色名县。境内有清西陵、狼牙山、易水湖、荆轲塔、云蒙山、燕下都、紫荆关、老子道德经幢等著名景点。
+                    在中国最美的乡村的评选中，易县为河北省唯一当选城市，
+                    同时获此殊荣的还有耳熟能详的云南丽江、大理，山西平遥等。
+                </p>
+            </div>
         </div>
-        
-        
+      
         <p class="split">易县旅游热门目的地</p>
         <div class="m-nearby">
             
         <ul>
             <li>
-                <img src="Public/img/易水湖3.jpg" height="128" width="224">
+                <img src="Public/img/易水湖.jpg" height="128" width="224">
                 <h3 class="name">易水湖</h3>
             </li>
             <li>
-                <img src="Public/img/易水湖3.jpg" height="128" width="224">
+                <img src="Public/img/清西陵.png" height="128" width="224">
                 <h3 class="name">清西陵</h3>
             </li>
             <li>
-                <img src="Public/img/易水湖3.jpg" height="128" width="224">
+                <img src="Public/img/狼牙山.png" height="128" width="224">
                 <h3 class="name">狼牙山</h3>
             </li>
             <li>
-                <img src="Public/img/易水湖3.jpg" height="128" width="224">
+                <img src="Public/img/云蒙山.png" height="128" width="224">
                 <h3 class="name">云蒙山</h3>
             </li>
             <li>
-                <img src="Public/img/易水湖3.jpg" height="128" width="224">
-                <h3 class="name">紫荆关</h3>
-            </li>
-            <li>
-                <img src="Public/img/易水湖3.jpg" height="128" width="224">
+                <img src="Public/img/荆轲塔.png" height="128" width="224">
                 <h3 class="name">荆轲塔</h3>
             </li>
             <li>
-                <img src="Public/img/易水湖3.jpg" height="128" width="224">
+                <img src="Public/img/千佛山.png" height="128" width="224">
                 <h3 class="name">千佛山森林公园</h3>
             </li>
             <li>
-                <img src="Public/img/易水湖3.jpg" height="128" width="224">
+                <img src="Public/img/洪崖山.png" height="128" width="224">
                 <h3 class="name">洪崖山</h3>
+            </li>
+            <li>
+                <img src="Public/img/易县战国影视城.png" height="128" width="224">
+                <h3 class="name">易县战国影视城</h3>
             </li>
         </uL>  
         </div>
