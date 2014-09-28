aa12dfa8a25b1d22a954faba750d3507b14724e7
veranderen <pufan.cn@gmail.com>
2014-09-28 23:13:58 +0800

    增加测试和天气页面

---
+++ Application/Home/View/weather/weather.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+http://www.weather.com.cn/data/sk/101090212.html
+//{"weatherinfo":{"city":"易县","cityid":"101090212","temp":"17","WD":"东南风","WS":"1级","SD":"99%","WSE":"1","time":"23:35","isRadar":"0","Radar":"","njd":"暂无实况","qy":"1007"}}
